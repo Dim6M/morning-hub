f0f09ad52a3b52124d230197a482aa8974b7b644
Dimitri <dmichel971@gmail.com>
2019-08-27 17:27:09 +0200

    add api call to get current weather

--- src/app/weather/weather.service.ts
+++ src/app/weather/weather.service.ts
@@ -6,5 +6,9 @@ import { HttpClient } from "@angular/common/http";
 })
 export class WeatherService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getCurrentWeather(location: string) {
+    return this.http.get(`http://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=7427b028cf8c2d818dc843e074039a4a`)
+  }
 }
