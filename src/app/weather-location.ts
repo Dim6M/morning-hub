bb7ab4f94ded0a056ed22a1d572ffe2dc7d0be17
Dim6M <dmichel971@gmail.com>
2019-09-07 05:03:31 +0200

    change weatherlocation attributes

--- src/app/weather-location.ts
+++ src/app/weather-location.ts
@@ -1,9 +1,11 @@
-export interface FavLocation {
+export interface WeatherLocation {
     location: string;
     temperature: number;
+    description: string;
     /* tempMin: number;
     tempMax: number;
     wind: number;
     sunrise: Date;
     sunset: Date; */
+    icon: string;
 }
